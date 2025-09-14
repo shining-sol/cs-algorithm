483a293f5e1ffd3eccecfeec65cb586d6d0e65b3
ChoiYoonSoo <gews30025@naver.com>
2025-09-15 04:48:57 +0900

    Add files via upload

---
+++ Algorithm/ChoiYoonSoo/폰켓몬.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i], map.get(nums[i]) + 1);
+            }
+            else{
+                map.put(nums[i], 1);
+            }
+        }
+
+        if(map.size() >= nums.length / 2){
+            return nums.length / 2;
+        }
+
+
+        return map.size();
+    }
+}
